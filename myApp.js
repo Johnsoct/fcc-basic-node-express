d31963e39ad13ffa8453125b1b3cd3f7e4b45a96
Taylor Johnson <johnsoctbus@gmail.com>
2024-10-12 09:43:02 -0400

    added /json endpoint

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+
+app.use('/public', express.static(`${__dirname}/public`))
 
 app.get('/', function (req, res) {
   res.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.get('/json', function (req, res) {
+  res.json({ message: 'Hello json' })
 })
 
 module.exports = app;
