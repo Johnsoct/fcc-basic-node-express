8f2a94f87e65f3f038b40b7562e5db52bf7f9429
Taylor Johnson <johnsoctbus@gmail.com>
2024-10-13 08:56:07 -0400

    Added body parser; new GET routes and a post route; project "complete"

--- myApp.js
+++ myApp.js
@@ -1,9 +1,24 @@
+// Packages
 require('dotenv').config()
-
+const bodyParser = require('body-parser')
 const express = require('express')
+
 const app = express()
 
 app.use('/public', express.static(`${__dirname}/public`))
+
+app.use(function (req, res, next) {
+  console.log(req.ip)
+  const method = req.method
+  const path = req.url
+  const ip = req.ip
+  
+  console.log(`${method} ${path} - ${ip}`)
+
+  next()
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
   res.sendFile(`${__dirname}/views/index.html`)
@@ -18,6 +33,32 @@ app.get('/json', function (req, res) {
   }
 
   res.json({ message, })
+})
+
+app.get(
+  '/now', 
+  function (req, res, next) {
+    req.time = new Date().toString()
+    next()
+  },
+  function (req, res) {
+    res.send({ time: req.time })
+  }
+)
+
+app.get('/:word/echo', function (req, res) {
+  res.send({ echo: req.params.word })
+})
+
+app.get('/name', function (req, res) {
+  const first = req.query.first
+  const last = req.query.last
+  
+  res.json({ name: `${first} ${last}` })
+})
+
+app.post('/name', function (req, res) {
+  res.json({ name: `${req.body.first} ${req.body.last}` })
 })
 
 module.exports = app
